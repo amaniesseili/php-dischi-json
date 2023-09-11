5af154415765d0911bdb4c2ed4b7ce1ada16d690
amani esseili <esseiliamani@hotmail.com>
2023-09-11 20:15:45 +0200

    creazione file json

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/css/bootstrap.min.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <title>PHP Dischi JSON</title>
 </head>
 <body>
-  
+  <h1>PHP Dischi JSON</h1>
+
+
+
+  <script src="main.js"></script>
 </body>
 </html>
